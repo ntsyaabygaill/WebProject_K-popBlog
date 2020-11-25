c28c8f98e54737f61f781a0c7d58ca6d9880bcf6
ntsyaabygaill <65441910+ntsyaabygaill@users.noreply.github.com>
2020-11-25 16:03:23 +0700

    vers 1

---
+++ Kpop/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+   
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"> <img src="images/KoBlogz.gif" width = "100 px" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto ">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">K-Pop<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">K-Drama</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Group
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Girl Group</a>
+                <a class="dropdown-item" href="#">Boy Group</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div>
+        <ul class="navbar-nav float-right mr-4 mt-2 mt-lg-0 ">
+              <li class="nav-item active">
+                <a class="nav-link" href="register.php">Sign Up<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="login.php">Login</a>
+              </li>
+            </ul>
+</div>
+      </nav>
---
+++ Kpop/index.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'?>
+
+<p>INI KONTENNYA WOI</p>
+
+<?php include 'footer.php'?>    
